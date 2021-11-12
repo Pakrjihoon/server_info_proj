da0c8e32973b238793aac74f3d76fcfad738b5c9
Pakrjihoon <wlgns8852@naver.com>
2021-11-12 11:43:40 +0900

    Rest-Api init 및 불필요 파일 제거

--- src/main/java/com/airi/education/domain/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.airi.education.domain.todo.repository;
-
-import com.airi.education.domain.todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO todo VALUES(1, 'Hello Content1', false, 'Hello Title1');
-INSERT INTO todo VALUES(2, 'Hello Content2', false, 'Hello Title2');
-INSERT INTO todo VALUES(3, 'Hello Content3', false, 'Hello Title3');
-INSERT INTO todo VALUES(4, 'Hello Content4', false, 'Hello Title4');
-INSERT INTO todo VALUES(5, 'Hello Content5', false, 'Hello Title5');
