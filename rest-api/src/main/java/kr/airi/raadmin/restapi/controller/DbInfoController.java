5cf17a0854837ecb5b220f1652fb446e3a27d35c
Pakrjihoon <wlgns8852@naver.com>
2021-12-21 18:19:01 +0900

    rabbitmq 매매 정보 post 추가

--- rest-api/src/main/java/kr/airi/raadmin/restapi/controller/DbInfoController.java
+++ rest-api/src/main/java/kr/airi/raadmin/restapi/controller/DbInfoController.java
@@ -3,17 +3,22 @@ package kr.airi.raadmin.restapi.controller;
 import kr.airi.raadmin.restapi.dto.DbinfoReqDto;
 import kr.airi.raadmin.restapi.entity.DbInfo;
 import kr.airi.raadmin.restapi.service.DbInfoService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/api")
-public class DbInfoController {
+public class DbInfoController implements Serializable {
 
     private DbInfoService dbInfoService;
+    private static final String EXCHANGE_NAME = "amq.fanout";
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
     @Autowired
     DbInfoController(DbInfoService dbInfoService) {
@@ -29,6 +34,7 @@ public class DbInfoController {
     @PostMapping(value = "/dbinfo")
     @ResponseBody
     public DbInfo saveDbInfo(@RequestBody DbinfoReqDto req) {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, "", req.toString());
         return dbInfoService.createOne(req);
     }
 
